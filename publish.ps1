bad1807ecd75914bd1d75d30d3e4dd243a519fcf
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2020-01-10 12:31:51 +0000

    Updated nuget publish

---
+++ publish.ps1
@@ -0,0 +1,21 @@
+if (!(Test-Path ".nuget\nuget.exe")) {
+	New-Item -ItemType directory -Path .nuget
+	
+	$MyFolder=get-item .\.nuget -Force
+	$MyFolder.attributes="Hidden"
+	
+	Invoke-WebRequest -OutFile .nuget\nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+}
+
+if (!(Test-Path "bin\Release")) {
+	Remove-Item bin\Release\*.nupkg
+}
+
+dotnet build -c Release
+
+dotnet pack -c Release
+
+# .nuget\nuget push *.nupkg -Source "https://nuget.org"
+
+Remove-Item bin\Release\*.nupkg
+
